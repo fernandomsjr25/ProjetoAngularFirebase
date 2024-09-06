2b7f4dbea173f490305cad2861581e6b41bd1baa
fernandomsjr <162391872+Fernandomsjr@users.noreply.github.com>
2024-09-06 23:05:57 +0000

    componente adicionado

---
+++ src/app/shared/incrementer/incrementer.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { IncrementerService } from '../services/incrementer.service';
+
+@Component({
+  selector: 'app-incrementer',
+  templateUrl: './incrementer.component.html',
+  styleUrls: ['./incrementer.component.scss'],
+})
+export class IncrementerComponent  implements OnInit {
+
+  valor: number = 0;
+
+  constructor(
+    public incrementer:IncrementerService
+  ) { }
+
+  ngOnInit() {}
+
+  decrementar() {
+    if (this.incrementer.valor > 0)
+      this.incrementer.valor--;
+  }
+
+  incrementar() { 
+    this.incrementer.valor++;
+  }
+
+  
+
+}

0054300dc858e5245af46e105531ae5641d491a5
fernandomsjr <162391872+Fernandomsjr@users.noreply.github.com>
2024-09-12 22:45:06 +0000

    ggggg

--- src/app/ver-detalhes/ver-detalhes.page.ts
+++ src/app/ver-detalhes/ver-detalhes.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, Routes } from '@angular/router';
 import { Console } from 'console';
 
 @Component({
@@ -12,10 +13,37 @@ export class VerDetalhesPage  {
 pais:any;
 tempo:any;
 
-  constructor() {
+  constructor(
+    private router:Router
+  ) {
     console.log(history.state.codigo)
     this.getPaisDetalhes();
    }
+
+
+   verDetalhes(pais:any){
+    
+    fetch(`https://restcountries.com/v3.1/alpha/${pais}`)
+    .then(dados=> dados.json( )  )
+    
+    .then(dados=> {
+      console.log(dados);
+      this.pais = dados;
+    })
+    
+    .catch(_=> {
+    console.log(_)
+    })
+    
+    .finally(()=>{
+      console.log('finalizado!')
+    })
+
+
+    }
+
+
+   
 
 getPaisDetalhes(){
 

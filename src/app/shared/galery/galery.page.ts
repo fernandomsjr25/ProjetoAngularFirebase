81f468a2a3f89742184b550a4e4933c0164c5848
fernandomsjr <162391872+Fernandomsjr@users.noreply.github.com>
2024-09-26 20:07:27 +0000

    pronto

--- src/app/shared/galery/galery.page.ts
+++ src/app/shared/galery/galery.page.ts
@@ -8,6 +8,24 @@ import { Component, OnInit } from '@angular/core';
 export class GaleryPage implements OnInit {
 
   constructor() { }
+  treinos  = {
+    braco:[
+           {imagem:"https://i.pinimg.com/originals/d2/c1/40/d2c140a0c785db594687ea8c8747a723.gif", nome:"rosca alternada"},
+           {imagem:"https://i.pinimg.com/originals/67/b9/7a/67b97ad79f373c490263d1dea8c31e38.gif", nome:"rosca direta"}
+          ],
+          costa:[
+            {imagem:"https://www.mundoboaforma.com.br/wp-content/uploads/2020/12/costas-puxada-com-barra-no-pulley-1.gif", nome:"Pulley Frente"},
+            {imagem:"https://www.hipertrofia.org/blog/wp-content/uploads/2023/11/dumbbell-bent-over-row.gif", nome:"remada"}
+           ],
+           peito:[
+            {imagem:"https://www.mundoboaforma.com.br/wp-content/uploads/2020/12/supino-reto.gif", nome:"supino reto"},
+            {imagem:"https://www.hipertrofia.org/blog/wp-content/uploads/2023/09/dumbbell-fly.gif", nome:"crucifixo"}
+           ],
+           perna:[
+            {imagem:"https://grandeatleta.com.br/wp-content/uploads/2018/05/execucao-exercicio-agachamento.gif", nome:"agachamento"},
+            {imagem:"https://alcateiademonstros.com.br/wp-content/uploads/2022/09/Levantamento-terra-romeno.gif", nome:"Levantamento terra"}
+           ],
+  }
 
   ngOnInit() {
   }
